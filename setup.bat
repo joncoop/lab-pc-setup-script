448021f15511df58596fe185e66af73905a753ab
Jon Cooper <joncoop@users.noreply.github.com>
2023-05-30 12:14:33 -0400

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -2,21 +2,25 @@
 setlocal enabledelayedexpansion
 
 
+REM List of applications to uninstall (not working yet, maybe too dangerous)
+set "uninstallList="BlueJ" "Python" "Color Cop" "Google Drive" "Visual Studio Code"
+
 rem URLs of the installer files
 set "installers[0]=http://colorcop.net/tools/colorcop/colorcop-setup.exe"
 set "installers[1]=https://www.bluej.org/download/files/BlueJ-windows-510.msi"
 set "installers[2]=https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe"
-set "installers[3]=https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe"
+set "installers[3]=https://code.visualstudio.com/sha/download?build=stable&os=win32-x64"
+set "installers[4]=https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe"
 
-rem Application installer flags
-set "flags[0]=/silent /MERGETASKS=desktopicon"
-set "flags[1]=/qn"
+rem Installer flags
+set "flags[0]=/silent"
+set "flags[1]=/qn ALLUSERS=1 INSTALLASSOCIATIONS=1 INSTALLMENUSHORTCUT=1 INSTALLDESKTOPSHORTCUT=1"
 set "flags[2]=/quiet InstallAllUsers=1 PrependPath=1 Include_test=0"
-set "flags[3]=--silent --desktop_shortcut=false --gsuite_shortcuts=false"
-
+set "flags[3]=--quiet"
+set "flags[4]=--silent --desktop_shortcut=false --gsuite_shortcuts=false"
 
 rem User profiles to keep (must be separated by commas)
-set "profilesToKeep=Administrator,Public,setup,Temp"
+set "profilesToKeep=013Tech,Administrator,Default,jccooper,Public,setup,Temp"
 
 rem Python packages to install
 set "packages=beautifulsoup4 matplotlib nympy pygame pyinstaller python-docx requests"
@@ -24,10 +28,15 @@ set "packages=beautifulsoup4 matplotlib nympy pygame pyinstaller python-docx req
 REM List of desktop icons to keep
 set "keepIcons=HelpDesk.exe" "Audacity.lnk" "BlueJ.lnk" "Chrome.lnk" "Color Cop.lnk" "HelpDesk.exe" "Idle.lnk" "Microsoft Edge.lnk" "Visual Studio Code.lnk"
 
-set "doneAction=restart"  REM Possible values: restart, shutdown. No value will keep the user logged in.
+REM Possible values: restart, shutdown. No value will keep the user logged in.
+set "closingAction=restart"
 
 
 rem ******* END OF CONFIGURATION. DO NOT MODIFY BELOW THIS LINE! *******
+
+
+REM Uninstall code goes here. too dangerous to try right now.
+
 
 echo Downloading installer files...
 
@@ -36,12 +45,12 @@ set "installerDir=%scriptDir%installers"
 
 mkdir "%installerDir%"
 
-for /L %%i in (0,1,3) do (
+for /L %%i in (0,1,4) do (
     set "installers[%%i]=!installers[%%i]!"
     for %%F in ("!installers[%%i]!") do (
         set "filename=%%~nxF"
         echo Downloading installer %%i: !filename!
-        bitsadmin.exe /transfer "InstallerDownload_%%i" "!installers[%%i]!" "%installerDir%\!filename!"
+        bitsadmin.exe /transfer "!filename!" "!installers[%%i]!" "%installerDir%\!filename!"
         echo Installer %%i downloaded successfully.
         set "installerPaths[%%i]=!filename!"
     )
@@ -49,7 +58,7 @@ for /L %%i in (0,1,3) do (
 
 echo Installing applications...
 
-for /L %%i in (0,1,3) do (
+for /L %%i in (0,1,4) do (
     set "installerPath=%installerDir%\!installerPaths[%%i]!"
     set "flags[%%i]=!flags[%%i]!"
 
@@ -64,10 +73,14 @@ for /L %%i in (0,1,3) do (
     )
 )
 
-rem Create a Desktop shortcut for IDLE because the installer doesn't provide that option
+rem Create a Desktop shortcuts
+echo Creating desktop shortcuts
+
 copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Python 3.11\IDLE (Python 3.11 64-bit).lnk" "C:\Users\Public\Desktop"
 ren "C:\Users\Public\Desktop\IDLE (Python 3.11 64-bit).lnk" "IDLE.lnk"
-echo IDLE shortcut added to Desktop.
+copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Color Cop\Color Cop.lnk" "C:\Users\Public\Desktop"
+copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio Code\Visual Studio Code.lnk" "C:\Users\Public\Desktop"
+
 
 rem Delete the installer directory
 echo Deleting installer directory...
@@ -125,9 +138,12 @@ for /F "delims=" %%G in ('dir "C:\Users" /B /AD') do (
         if /I "%%G"=="%%P" set "skipProfile=1"
     )
     
+    rem Check if the profile is the current user's profile
+    if /I "%%G"=="%USERNAME%" set "skipProfile=1"
+
     rem Check if the profile has been modified within the past year
-    forfiles /P "C:\Users\%%G" /M * /D -365 /C "cmd /c exit 1" >nul 2>&1
-    if not errorlevel 1 set "skipProfile=1"
+    forfiles /P "C:\Users\%%G" /M * /D -365 /C "cmd /c exit 0" >nul 2>&1
+    if errorlevel 1 set "skipProfile=1"
 
     rem Delete profiles
     if not defined skipProfile (
@@ -138,16 +154,14 @@ for /F "delims=" %%G in ('dir "C:\Users" /B /AD') do (
     )
 )
 
-endlocal
 
-REM Perform the specified action
-if "%doneAction%"=="restart" (
+REM Perform the specified closing action
+if "!closingAction!"=="restart" (
     echo Restarting in 5 seconds...
     shutdown /r /t 5
-) else if "%doneAction%"=="shutdown" (
+) else if "!closingAction!"=="shutdown" (
     echo Shutting down in 5 seconds...
     shutdown /s /t 5
 ) else (
-    echo Invalid action specified. No further action taken.
     pause
 )
